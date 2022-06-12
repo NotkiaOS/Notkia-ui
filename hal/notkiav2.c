b1b3a36e42164d7047446487669130cc558d7b13
luka177 <lukapanio@gmail.com>
2022-06-12 12:03:45 +0300

    [hal:notkiav2] Initial driver

--- hal/notkiav2.c
+++ hal/notkiav2.c
@@ -12,7 +12,8 @@
 #include "lvgl/lvgl.h"
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
-#include "lv_drivers/sdl/sdl.h"
+#include "lv_drivers/display/fbdev.h"
+#include "lv_drivers/indev/evdev.h"
 
 /*********************
  *      DEFINES
@@ -45,7 +46,39 @@
 #if defined(TARGET_NOTKIAV2_HAL)
     void hal_init(void)
     {
-        //TODO: write stuff here
+        fbdev_init();
+        int height, width;
+        fbdev_get_sizes(&width, &height);
+        
+        /*Create a display buffer*/
+        lv_disp_draw_buf_t disp_buf1;
+        lv_color_t buf1_1[width * 100];
+        lv_disp_draw_buf_init(&disp_buf1, buf1_1, NULL, width * 100);
+
+        /*Create a display*/
+        static lv_disp_drv_t disp_drv;
+        lv_disp_drv_init(&disp_drv); /*Basic initialization*/
+        disp_drv.draw_buf = &disp_buf1;
+        disp_drv.flush_cb = fbdev_flush;
+        disp_drv.hor_res = width;
+        disp_drv.ver_res = height;
+
+        lv_disp_t * disp = lv_disp_drv_register(&disp_drv);
+
+        lv_theme_t * th = lv_theme_default_init(disp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK, LV_FONT_DEFAULT);
+        lv_disp_set_theme(disp, th);
+
+        lv_group_t * g = lv_group_create();
+        lv_group_set_default(g);
+        
+        evdev_init();
+        
+        static lv_indev_drv_t indev_drv_2;
+        lv_indev_drv_init(&indev_drv_2); /*Basic initialization*/
+        indev_drv_2.type = LV_INDEV_TYPE_KEYPAD;
+        indev_drv_2.read_cb = evdev_read;
+        lv_indev_t *kb_indev = lv_indev_drv_register(&indev_drv_2);
+        lv_indev_set_group(kb_indev, g);
     }
 #endif
 
